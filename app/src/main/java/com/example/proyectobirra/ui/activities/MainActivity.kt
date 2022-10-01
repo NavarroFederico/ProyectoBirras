1a67f212c946806b56dd7b4cb5d56b9225adfc2f
Federico Navarro <navarrofederico14@gmail.com>
2022-10-01 14:06:58 -0300

    initial commit dependencias agregadas

---
+++ app/src/main/java/com/example/proyectobirra/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.proyectobirra.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.proyectobirra.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
